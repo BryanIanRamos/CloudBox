8a6329556506eedd266365e2e58965d123033810
BryanIanRamos <103841965+BryanIanRamos@users.noreply.github.com>
2023-11-14 18:37:41 +0800

    add conify as dependency

--- src/Pages/Dashboard/components.js/ProfileHdr.js
+++ src/Pages/Dashboard/components.js/ProfileHdr.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProfileHdr = () => {
   return (
-    <div className="w-[100%] h-[45px]  bg-white shadow border border-white absolute text-center">
+    <div className="w-full  h-[45px]  bg-white shadow border border-white absolute text-center">
       Profile Bar
     </div>
   );
