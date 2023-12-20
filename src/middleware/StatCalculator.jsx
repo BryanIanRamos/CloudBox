1b1c34e06907f468234273de3b18d7632b97492b
BryanIanRamos <103841965+BryanIanRamos@users.noreply.github.com>
2023-12-21 02:10:38 +0800

    update StatCalculator

--- src/middleware/StatCalculator.jsx
+++ src/middleware/StatCalculator.jsx
@@ -3,12 +3,17 @@ import React, { useEffect, useState } from "react";
 const StatCalculator = (whole, part) => {
   //   const [percentage, setPercentage] = useState(0);
 
-  const result = (part / whole) * 100;
+  if (whole === 0 || part === 0) {
+    const percentage = 0;
+    return { percentage };
+  } else {
+    const result = (part / whole) * 100;
 
-  const percentage = parseInt(result);
+    const percentage = parseInt(result);
 
-  console.log(percentage);
-  return { percentage };
+    // console.log(percentage);
+    return { percentage };
+  }
 };
 
 export default StatCalculator;
